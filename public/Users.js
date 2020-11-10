ce2f262ecf5634101cc698e4ca69e38100870ab5
soorajks21 <soorajks@live.in>
2020-11-10 18:45:43 -0500

    adding routes for fetching user

---
+++ public/Users.js
@@ -0,0 +1,25 @@
+
+const users = [
+    {
+        id: 1,
+        name: 'Sooraj Suresh',
+        email: 'soorajks@live.in',
+        status:'active'
+    },
+    {
+        id: 2,
+        name: 'Vishnu Sasi',
+        email: 'vishnu@gmail.com',
+        status:'active'
+    },
+    {
+        id: 3,
+        name: 'Ragesh Sasi',
+        email: 'vishnu@gmail.com',
+        status:'inactive'
+    }
+
+
+]
+
+module.exports = users;
